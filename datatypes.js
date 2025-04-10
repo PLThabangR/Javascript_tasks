3150efed325c907bf81db0a82b337ee8ef8b4aad
plthabangr <rakgoropothabang@gmail.com>
2025-04-11 01:06:31 +0200

    Added different file tasks

---
+++ datatypes.js
@@ -0,0 +1,71 @@
+//Data types and operators 
+//Execise1 numbers
+
+let  num1 = 10
+let num2 = 5.5
+
+//addition
+addition= num1+num2
+//subtraction
+subtraction= num1-num2
+//multiplication
+multiplicatio= num1*num2
+//division
+division = num1/num2
+//Modulus
+modulus = num1%num2
+//Modulus
+
+
+console.log(num1," + ",num2," = ",addition)
+console.log(num1," - ",num2," = ",subtraction)
+console.log(num1," * ",num2," = ",multiplicatio)
+console.log(num1," / ",num2," = ",division)
+console.log(num1," % ",num2," = ",modulus)
+
+
+//Boolean and operators
+
+result1 = 10>40 
+result2= 10>=40 
+result2 = 10<=40 
+result3 = 10<40 
+result4 = 10==40 
+result5 = 10===40 
+result6 = 10!=40 
+
+//Declaring variable x and y
+let x =8
+let y =12
+
+console.log("x greater than y ",x>y)
+console.log("x kess than or equal to y ",x<=y)
+console.log("x is equal to y ",x==y)
+console.log("x is not equal to y ",x!=y)
+
+//declaring variable a nd b
+let a= true 
+let b = false
+
+console.log(a && b)
+console.log(a || b)
+console.log(a != b)
+
+
+//
+//Declare variable p and assign it a value of 10.
+console.log("-----------")
+let p =10
+
+p+=6
+console.log(p)
+p-=6
+console.log(p)
+p*=6
+console.log(p)
+p/=6
+console.log(p)
+p%=6
+console.log(p)
+
+
---
+++ variables.js
@@ -0,0 +1,44 @@
+//Variable declaration
+
+const name ="Mike"
+const age = 30
+isStudent =true
+
+console.log("Student Name ",name)
+console.log("Student age ",age)
+console.log("Is student studying ",isStudent)
+
+
+
+//Variable reassignment
+
+let favoriteColour ="blue"
+console.log(favoriteColour)
+favoriteColour = "red"
+console.log(favoriteColour)
+
+
+//Arithmetic
+
+let num1 =10
+let num2 =5
+
+let sum =0 
+let difference =0
+let  product =0
+let  quotient =0
+
+//sum
+sum= num1+num2
+//difference
+difference= num1-num2
+//product
+product= num1*num2
+//quotient
+quotient = num1/num2
+
+console.log(num1," + ",num2," = ",sum)
+console.log(num1," - ",num2," = ",difference)
+console.log(num1," * ",num2," = ",product)
+console.log(num1," / ",num2," = ",quotient)
+
