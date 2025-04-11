9eb739c21702e5bcd5371b0d4dac087bd532aae3
plthabangr <rakgoropothabang@gmail.com>
2025-04-11 03:28:04 +0200

    Number line jump test passed

--- NumberLineJumps.js
+++ NumberLineJumps.js
@@ -8,7 +8,7 @@ if(v1==v2){
 return "YES"
 }else if(x1==x2){
         return "NO"
-}else if((x2-x1)%(v1-v2)==0 && (x2-x1)/(v1-v2)>0){
+}else if(((x2-x1)%(v1-v2)==0) && ((x2-x1)/(v1-v2)>0)){
     return "YES"
 }else{
     return "NO"
