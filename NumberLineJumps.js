b8283975b787cd51c797b5d6bcce919df8bb07de
plthabangr <rakgoropothabang@gmail.com>
2025-04-11 03:11:42 +0200

    Completed test cases for number line jumps

---
+++ NumberLineJumps.js
@@ -0,0 +1,18 @@
+//Number line jump
+//Sample code for solution
+// 
+ function kangaroo(x1, v1, x2, v2) {
+    // Write your code here
+    
+if(v1==v2){
+return "YES"
+}else if(x1==x2){
+        return "NO"
+}else if((x2-x1)%(v1-v2)==0 && (x2-x1)/(v1-v2)>0){
+    return "YES"
+}else{
+    return "NO"
+}
+     
+    
+}
